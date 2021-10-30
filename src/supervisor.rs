ec185ecb55f8b9ab3dbca34d0780ba2a595d439d
algon-320 <algon.0320@gmail.com>
2021-10-30 13:50:47 +0900

    Improve supervisor
    - Stop retrying on frequent failure

--- src/supervisor.rs
+++ src/supervisor.rs
@@ -1,15 +1,36 @@
-use log::info;
+use log::{error, info};
 use std::process::Command;
+use std::time::Instant;
 
 const DAILY_BIN_NAME: &str = "daily";
 
 fn main() {
+    env_logger::init();
+
+    let mut retries = 0;
     loop {
         info!("Try to launch a Daily process.");
         let mut ch = Command::new(DAILY_BIN_NAME).spawn().unwrap();
+        let started_time = Instant::now();
+
+        // Exit caused by user.
         if ch.wait().unwrap().success() {
             info!("Daily has gracefully exited.");
             break;
         }
+
+        if started_time.elapsed().as_millis() < 3000 {
+            retries += 1;
+
+            // Maybe something is wrong in launching process.
+            if retries > 10 {
+                error!("Frequent failure");
+                return;
+            }
+        } else {
+            retries = 0;
+        }
+
+        info!("uptime: {} secs.", started_time.elapsed().as_secs());
     }
 }
